3fa4c6cece5924a423e076cd84b13442be934205
Etsnetizen <47016498+Etsnetizen@users.noreply.github.com>
2019-10-02 12:21:33 +0800

    小程序增加是否超过最大报障限制的检查

--- mini_application-1.0/pages/application/index.js
+++ mini_application-1.0/pages/application/index.js
@@ -7,10 +7,12 @@ Page({
         attribute:{
             '0':'正在获取',
             '1':'正在获取'
-        }
+        },
+       maxsize_report_index:false
    },
    onLoad:function(){
-       this.ConnectToDataBase()
+       this.ConnectToDataBase();
+       this.CheckMaxsizeReport()
     },
    onShow:function () {
         var that = this;
@@ -46,8 +48,24 @@ Page({
        wx.navigateTo({
            url:'/pages/application/tea?id=' + e.currentTarget.dataset.id
        })
-   }
-
+   },
+   CheckMaxsizeReport:function () {
+        wx.showLoading();
+        var that = this;
+        wx.request({
+            url:app.buildUrl('/index/check'),
+            header:app.getRequestHeader(),
+            success:function ( res ) {
+                var resp = res.data;
+                if( resp.code == 200 ){
+                    that.setData({
+                        maxsize_report_index:true
+                    })
+                }
+                wx.hideLoading();
+            }
+        });
+    }
 });
 
 
